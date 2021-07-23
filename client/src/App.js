85e7eea9e5278e73db794b494d9f6e000cb77ae2
Kyle Friel <businessfriel@gmail.com>
2021-07-22 23:57:41 -0400

    First commit.

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex molestiae blanditiis
+            necessitatibus, omnis hic doloribus officia saepe alias aperiam. Corporis temporibus
+            laboriosam maxime, unde corrupti accusamus illum? Mollitia, ad.
+        </div>
+    );
+}
+
+export default App;
