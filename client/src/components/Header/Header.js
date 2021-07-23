0860839ed7f0e808741a70f9b363bb1e0d8ede82
Kyle Friel <businessfriel@gmail.com>
2021-07-23 11:44:17 -0400

    Header and nav menu functionality and styling.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { CgMenuRight, CgClose } from "react-icons/cg";
 import Button from "../../UI/Button/Button";
 
 import classes from "./Header.module.scss";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen((p) => !p);
+    };
+
     return (
         <header className={classes.header}>
             <nav className={classes.nav}>
                 <div className={classes.nav__logo}>
                     <h1>dev4real</h1>
                 </div>
-                <div className={classes.nav__links}>
+                <div
+                    className={`${classes.nav__links} ${
+                        menuOpen && classes["nav__links--display"]
+                    }`}
+                >
                     <ul>
                         <li>
                             <a href="/">About</a>
@@ -28,6 +39,9 @@ const Header = () => {
                     <div>
                         <Button>resume</Button>
                     </div>
+                </div>
+                <div className={classes.nav__toggler} onClick={handleMenuToggle}>
+                    {menuOpen ? <CgClose /> : <CgMenuRight />}
                 </div>
             </nav>
         </header>
