108a5f10c2ebc014a8d0dc4cd601bbe727705579
Kyle Friel <businessfriel@gmail.com>
2021-07-24 12:59:14 -0400

    Project component structure and styles.

--- client/src/UI/Card/Card.js
+++ client/src/UI/Card/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import classes from "./Card.module.scss";
 
-const Card = ({ children }) => {
-    return <div className={classes.card}>{children}</div>;
+const Card = ({ children, className }) => {
+    return <div className={`${classes.card} ${className}`}>{children}</div>;
 };
 
 export default Card;
--- client/src/components/sections/Projects/Project/Project.js
+++ client/src/components/sections/Projects/Project/Project.js
@@ -1,11 +1,37 @@
 import React from "react";
-
+import Card from "./../../../../UI/Card/Card";
 import classes from "./Project.module.scss";
+
+import { FaGithub } from "react-icons/fa";
+import { BiLinkExternal } from "react-icons/bi";
 
 const Project = () => {
     return (
         <li className={classes.project}>
-            <div className={classes.project__content}></div>
+            <div className={classes.project__content}>
+                <h6 className={classes.project__content__pre}>Project Spotlight</h6>
+                <h3 className={classes.project__content__title}>
+                    <a href="/">Sorting Visualizer</a>
+                </h3>
+                <Card className={classes.project__content__description}>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In repellat
+                        dolorem facere tempore inventore velit eligendi ad amet voluptates libero.
+                    </p>
+                </Card>
+                <ul className={classes.project__content__tech}>
+                    <li>React</li>
+                    <li>Sass</li>
+                </ul>
+                <div className={classes.project__content__links}>
+                    <a href="/">
+                        <FaGithub />
+                    </a>
+                    <a href="/">
+                        <BiLinkExternal />
+                    </a>
+                </div>
+            </div>
             <div className={classes.project__image}>
                 <a href="/">
                     <div className={classes.img__wrap}>
