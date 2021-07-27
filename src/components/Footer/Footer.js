bc2f66cedc194460df715a23dcceb3f0de697190
Kyle Friel <businessfriel@gmail.com>
2021-07-27 02:01:51 -0400

    README updated. Footer github icon linked to this project's github repo.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,14 @@ const Footer = () => {
             <div className={classes.footer}>
                 <Logo className={classes.footer__logo} />
                 <h4 className={classes.footer__tagline}>Designed and Built by Kyle Friel</h4>
-                <FaGithub className={classes.footer__link} />
+                <a
+                    href="https://github.com/kyle4real/Portfolio-v1"
+                    target="_blank"
+                    rel="noreferrer"
+                    className={classes.footer__link}
+                >
+                    <FaGithub />
+                </a>
             </div>
         </SectionLayout>
     );
