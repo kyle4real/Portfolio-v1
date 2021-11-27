5dc279bc9a7ee865d5a60045882d015b75978931
Kyle Friel <businessfriel@gmail.com>
2021-11-27 13:53:47 -0500

    Removed github pages setup. Updated techologies list.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This portfolio was built using React and it's purpose is to display Kyle Friel's
 
 The App is deployed with Github pages and can be accessed here: [https://kyle4real.github.io/Portfolio-v1/].
 
-## Design
+## Build Tools
 
-The design process started out with a mixture of layout brainstorming, inspiration seeking via a few google searches, and using a nifty color palette generator to help turn my unhinged color theme ideas into a harmonized set of hex codes. I decided to use scss so I could easily access the color codes, breakpoint mixin, and other crucial variables across all components. Plus, I enjoy the convenient nesting feature that scss offers.
+This site was built with React and SCSS.
--- src/components/sections/About/About.js
+++ src/components/sections/About/About.js
@@ -14,47 +14,9 @@ const technologies = [
     { name: "ReactJS", img: <FaReact className={classes.react} /> },
     { name: "Node.js", img: <FaNodeJs /> },
     { name: "MongoDB", img: <SiMongodb /> },
-    { name: "Hello, World", img: null },
-    { name: "Express", img: null },
-    { name: "C", img: null },
-    { name: "Python", img: null },
-    { name: "VSCode", img: null },
-    { name: "ExtendScript", img: null },
-    { name: "TypeScript", img: null },
-    { name: "Redux", img: null },
-    { name: "@redux/toolkit", img: null },
-    { name: "React Context", img: null },
-    { name: "SCSS", img: null },
-    { name: "Mongoose", img: null },
-    { name: "Mongo Atlas", img: null },
-    { name: "Cloudinary", img: null },
-    { name: "NextJS", img: null },
-    { name: "Material UI", img: null },
-    { name: "Bootstrap", img: null },
-    { name: "Tailwind", img: null },
-    { name: "Heroku", img: null },
-    { name: "Netlify", img: null },
-    { name: "styled-components", img: null },
-    { name: "Shopify", img: null },
-    { name: "react-router-dom", img: null },
-    { name: "photoshop scripting", img: null },
-    { name: "git/github", img: null },
-    { name: "GraphQL", img: null },
-    { name: "Firebase", img: null },
-    { name: "Amazon S3", img: null },
-    { name: "jsonwebtoken", img: null },
 ];
 
 const About = () => {
-    const [input, setInput] = useState("showcase");
-
-    const filteredTechnologies = useMemo(() => {
-        if (input === "showcase" || input === "pretty cool, huh?" || input === "") return null;
-        if (input === "all") return technologies;
-        return technologies.filter(({ name }) => name.toLowerCase().includes(input.toLowerCase()));
-    }, [input]);
-
-    const output = filteredTechnologies ? filteredTechnologies : technologies.slice(0, 6);
     return (
         <div className={classes.bgcontrol}>
             <div className={classes["about__section-BGtop"]} />
@@ -82,21 +44,10 @@ const About = () => {
                         <div className={classes.technologies__header}>
                             <span className={classes.arrow}>&lt;</span>
                             <span className={classes.label__white}>technologies</span>
-                            <span className={classes.label__orange}>&nbsp;class</span>
-                            <span className={classes.label__teal}>="</span>
-
-                            <input
-                                className={classes.technologies__input}
-                                onChange={(e) => setInput(e.target.value)}
-                                value={input}
-                                onBlur={() => setInput("pretty cool, huh?")}
-                            />
-
-                            <span className={classes.label__teal}>"</span>
                             <span className={classes.arrow}>&gt;</span>
                         </div>
                         <div className={classes.technologies__grid}>
-                            {output.map(({ name, img }, index) => (
+                            {technologies.map(({ name, img }, index) => (
                                 <Card className={classes.technology__card} key={index}>
                                     <div className={classes.technology__img}>{img && img}</div>
                                     <h2 className={classes.technology__name}>{name}</h2>
